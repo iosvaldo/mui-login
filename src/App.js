8df1e41794f45a35614eb3f260cb9125584bc0b1
osvaldo <iosvaldo.app@gmail.com>
2023-02-02 18:31:12 -0500

    complete mui login page 📟

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import loginBG from "./assets/gradient.jpg";
+import LoginContainer from './components/LoginContainer';
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  textAlign: 'center',
+  height: "100vh",
+  color: theme.palette.text.secondary,
+}));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container >
+          <Grid item xs={6}>
+            <Item>
+              <img src={loginBG} width="100%" height="100%" alt="login_bg" />
+              
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}>
+              <LoginContainer />
+            </Item>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
---
+++ src/components/LoginContainer.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Container } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import  Button  from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { CgLogIn } from "react-icons/cg"
+import logo from "../assets/login_logo.png";
+
+function LoginContainer() {
+
+ 
+  return (
+    <Container
+      style={{
+        width: "400px",
+        padding: "2em",
+        borderRadius: "5px",
+        boxShadow: "-1px -1px 27px -6px rgba(0,0,0,0.75)",
+      }}
+    >
+      <img style={{ width: "10%", height: "10%" }} src={logo} alt="logo" />
+      <div
+        className="login-container"
+        style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}
+      >
+        <Typography
+          style={{
+            display: "flex",
+            alignItems: "center",
+            fontFamily: "Montserrat",
+          }}
+          variant="h3"
+          color={"#000"}
+          gutterBottom
+        >
+          <CgLogIn
+            style={{
+              padding: "10px",
+              color: "grey",
+            }}
+          />
+          Login
+        </Typography>
+      </div>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "15vw" },
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          type={"password"}
+        />
+      </Box>
+
+      <Button
+        style={{ backgroundCOlor: "#345", width: "15vw", marginTop: "1em" }}
+        variant="contained"
+      >
+        Login
+      </Button>
+    </Container>
+  );
+}
+
+
+export default LoginContainer
